9eb6a558434a9d3c10b01cb07f38ef7b16e56095
mostovoy.nikolay@gmail.com <mostovoy.nikolay@gmail.com>
2018-06-18 05:51:22 +0000

    fixing bugs

--- index.js
+++ index.js
@@ -20,12 +20,10 @@ const _ = require('lodash')
 const spawn = require('child_process').spawn
 
 const gpu_temp_command = 'sudo /opt/vc/bin/vcgencmd measure_temp'
-const cpu_temp_command = 'cat /sys/class/thermal/thermal_zone0/temp'
-const cpu_util_default_command = 'top -n1 \| awk \'\/Cpu\\\(s\\\)\:\/ \{print $2\}\''
-const cpu_util_mpstat_command = 'mpstat -P ALL'
-const mem_util_command = ''
-const sd_util_command = ''
-const swap_util_command =''
+const cpu_temp_command = 'sudo cat /sys/class/thermal/thermal_zone0/temp'
+const cpu_util_mpstat_command = 'sudo mpstat -P ALL\|grep \\\:\|grep -v \\\%'
+const mem_util_command = 'sudo free'
+const sd_util_command = 'df \/\|grep -v Used\|awk \'\{print \$5\}\'\|awk \'gsub\(\"\%\"\,\"\"\)\''
 
 module.exports = function(app) {
   var plugin = {};
@@ -67,14 +65,8 @@ module.exports = function(app) {
       path_sd_util: {
         title: "SignalK Path for SD card utilisation(%)",
         type: "string",
-        default: "environment.rpi.cpu.utilisation",
+        default: "environment.rpi.sd.utilisation",
       },
-      path_cpu_util: {
-        title: "SignalK Path for swap utilisation(Mb)",
-        type: "string",
-        default: "environment.rpi.cpu.utilisation",
-      },
-
       rate: {
         title: "Sample Rate (in seconds)",
         type: 'number',
@@ -91,6 +83,8 @@ module.exports = function(app) {
       getGpuTemperature()
       getCpuTemperature()
       getCpuUtil()
+      getMemUtil()
+      getSdUtil()
     }
 
     function getGpuTemperature() {
@@ -166,54 +160,50 @@ module.exports = function(app) {
     }
 
     function getCpuUtil() {
-      var cpuUtilReady = 0
       var processcpuutilfull = spawn('sh', ['-c', cpu_util_mpstat_command ])
 
       processcpuutilfull.stdout.on('data', (data) => {
         debug(`got cpu utilisation  ${data}`)
-        var re = /cpu/im
+        var re = /all/im
         if (data.toString().match(re)) {
-          cpuUtilReady = 1
-          var cpu_util = data.toString().split('\n')
+          var cpu_util = data.toString().replace(/(\n|\r)+$/, '').split('\n')
           cpu_util.forEach(function(cpu_util_line){
-            spl_line = cpu_util_line.split('\s+')
-            var re2 = /^[0-9]?/
-            if (spl_line[2].match(re2)) {
-              if (spl_line[1].match(re2)){
-                debug(`cpu utilisation core ${spl_line[1]} is ${spl_line[11]}`)
-                var newPath
-                pathArray = options.path_cpu_util.split('.')
-                for (i=0; i < (pathArray.lenght -2); i++) {
-                  newPath = newPath + pathArray[i]
-                }
-                newPath = newPath + ".core." + spl_line[1]
-                newPath = newPath + "." + pathArray[(pathArray.length-1)]
-                cpu_util_core = 100 - Number(spl_line[11])
-                app.handleMessage(plugin.id, {
-                  updates: [
-                    {
-                      values: [ {
-                        path: newPath,
-                        value: cpu_util_core
-                      }]
-                    }
-                  ]
-                })
+            var spl_line = cpu_util_line.replace(/ +/g, ' ').split(' ')
+            var re2 = /^[0-9]?$/
+            if (spl_line[1].match(re2)){
+              debug(`cpu utilisation core ${spl_line[1]} is ${spl_line[11]}`)
+              var pathArray = options.path_cpu_util.toString().split('\.')
+              var newPath = pathArray[0] + "."
+              for (i=1; i < (pathArray.length - 1); i++) {
+                newPath = newPath + pathArray[i].toString() +"."
               }
-              else {
-                debug(`cpu utilisation is ${spl_line[11]}`)
-                cpu_util_all = 100 - Number(spl_line[11])
-                app.handleMessage(plugin.id, {
-                  updates: [
-                    {
-                      values: [ {
-                        path: options.path_cpu_util,
-                        value: cpu_util_all
-                      }]
-                    }
-                  ]
-                })
-              }
+              newPath = newPath + "core." + (Number(spl_line[1])+1).toString()
+              newPath = newPath + "." + pathArray[(pathArray.length-1)]
+              var cpu_util_core = (100 - Number(spl_line[11])).toFixed(0)
+              app.handleMessage(plugin.id, {
+                updates: [
+                  {
+                    values: [ {
+                      path: newPath,
+                      value: cpu_util_core
+                    }]
+                  }
+                ]
+              })
+            }
+            else {
+              debug(`cpu utilisation is ${spl_line[11]}`)
+              cpu_util_all = (100 - Number(spl_line[11])).toFixed(0)
+              app.handleMessage(plugin.id, {
+                updates: [
+                  {
+                    values: [ {
+                      path: options.path_cpu_util,
+                      value: cpu_util_all
+                    }]
+                  }
+                ]
+              })
             }
           })
         }
@@ -226,34 +216,66 @@ module.exports = function(app) {
       processcpuutilfull.stderr.on('data', function (data) {
         console.error(data.toString())
       })
+    }
 
-      if (cpuUtilReady != 1) {
-        var processcpuutildef = spawn('sh', ['-c', cpu_util_default_command ])
+    function getMemUtil() {
+      var processmemutil = spawn('sh', ['-c', mem_util_command ])
 
-        processcpuutildef.stdout.on('data', (data) => {
-          debug(`got cpu utilisation  ${data}`)
-          var cpu_util_def = Number(data)
-
-          app.handleMessage(plugin.id, {
-            updates: [
-              {
-                values: [ {
-                  path: options.path_cpu_util,
-                  value: cpu_util_def
-                }]
-              }
-            ]
-          })
+      processmemutil.stdout.on('data', (data) => {
+        debug(`got memory  ${data}`)
+        var mem_util = data.toString().replace(/(\n|\r)+$/, '').split('\n')
+        mem_util.forEach(function(mem_util_line){
+          var splm_line = mem_util_line.replace(/ +/g, ' ').split(' ')
+          if (splm_line[0].toString() === "Mem:"){
+            var mem_util_per = (Number(splm_line[2])/Number(splm_line[1])*100).toFixed(0)
+            app.handleMessage(plugin.id, {
+              updates: [
+                {
+                  values: [ {
+                    path: options.path_mem_util,
+                    value: mem_util_per
+                  }]
+                }
+              ]
+            })
+          }
         })
+      })
 
-        processcpuutildef.on('error', (error) => {
-          console.error(error.toString())
-        })
+      processmemutil.on('error', (error) => {
+        console.error(error.toString())
+      })
 
-        processcpuutildef.stderr.on('data', function (data) {
-          console.error(data.toString())
+      processmemutil.stderr.on('data', function (data) {
+        console.error(data.toString())
+      })
+    }
+
+    function getSdUtil() {
+      var processsdutil = spawn('sh', ['-c', sd_util_command ])
+
+      processsdutil.stdout.on('data', (data) => {
+        debug(`got sd  ${data}`)
+        var sd_util = data.toString().replace(/(\n|\r)+$/, '')
+        app.handleMessage(plugin.id, {
+          updates: [
+            {
+              values: [ {
+                path: options.path_sd_util,
+                value: sd_util
+              }]
+            }
+          ]
         })
-      }
+      })
+
+      processsdutil.on('error', (error) => {
+        console.error(error.toString())
+      })
+
+      processsdutil.stderr.on('data', function (data) {
+        console.error(data.toString())
+      })
     }
 
     updateEnv()
