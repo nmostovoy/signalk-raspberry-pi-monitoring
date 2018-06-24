f8fd75a41f9349c36fcddfb0630456c660790b01
mostovoy.nikolay@gmail.com <mostovoy.nikolay@gmail.com>
2018-06-24 11:24:19 +0000

    fixing bugs

--- index.js
+++ index.js
@@ -37,33 +37,28 @@ module.exports = function(app) {
     type: "object",
     description: "The user running node server must have permission to sudo without needing a password",
     properties: {
-      temp_unit: {
-        title: "Temperature unit (K - Kelvin(default), C - Celsius, F - Fahrenheit)",
-        type: "string",
-        default: 'K'
-      },
       path_cpu_temp: {
-        title: "SignalK Path for CPU temperature(degrees)",
+        title: "SignalK Path for CPU temperature (K)",
         type: "string",
         default: "environment.rpi.cpu.temperature",
       },
       path_gpu_temp: {
-        title: "SignalK Path for GPU temperature(degrees)",
+        title: "SignalK Path for GPU temperature (K)",
         type: "string",
         default: "environment.rpi.gpu.temperature",
       },
       path_cpu_util: {
-        title: "SignalK Path for CPU utilisation(%) (Please install sysstat for per core monitoring)",
+        title: "SignalK Path for CPU utilisation (Please install sysstat for per core monitoring)",
         type: "string",
         default: "environment.rpi.cpu.utilisation",
       },
       path_mem_util: {
-        title: "SignalK Path for memory utilisation(%)",
+        title: "SignalK Path for memory utilisation",
         type: "string",
         default: "environment.rpi.memory.utilisation",
       },
       path_sd_util: {
-        title: "SignalK Path for SD card utilisation(%)",
+        title: "SignalK Path for SD card utilisation",
         type: "string",
         default: "environment.rpi.sd.utilisation",
       },
@@ -88,18 +83,11 @@ module.exports = function(app) {
     }
 
     function getGpuTemperature() {
-      var processgputemp = spawn('sh', ['-c', gpu_temp_command ])
+      var gputemp = spawn('sh', ['-c', gpu_temp_command ])
 
-      processgputemp.stdout.on('data', (data) => {
+      gputemp.stdout.on('data', (data) => {
         debug(`got gpu  ${data}`)
-        var gpu_temp = Number(data.toString().split('=')[1].split('\'')[0])
-        if (options.temp_unit === 'F') {
-          gpu_temp = ((gpu_temp*9/5) + 32).toFixed(2)
-        }
-        else if (options.temp_unit === 'C') {}
-        else {
-          gpu_temp = gpu_temp + 273.15
-        }
+        var gpu_temp = (Number(data.toString().split('=')[1].split('\'')[0]) + 273.15).toFixed(2)
         debug(`gpu temp is ${gpu_temp}`)
 
         app.handleMessage(plugin.id, {
@@ -107,35 +95,28 @@ module.exports = function(app) {
             {
               values: [ {
                 path: options.path_gpu_temp,
-                value: gpu_temp
+                value: Number(gpu_temp)
               }]
             }
           ]
         })
       })
 
-      processgputemp.on('error', (error) => {
+      gputemp.on('error', (error) => {
         console.error(error.toString())
       })
 
-      processgputemp.on('data', function (data) {
+      gputemp.on('data', function (data) {
         console.error(data.toString())
       })
     }
 
     function getCpuTemperature() {
-      var processcputemp = spawn('sh', ['-c', cpu_temp_command ])
+      var cputemp = spawn('sh', ['-c', cpu_temp_command ])
 
-      processcputemp.stdout.on('data', (data) => {
+      cputemp.stdout.on('data', (data) => {
         debug(`got cpu  ${data}`)
-        var cpu_temp = (Number(data)/1000).toFixed(2)
-        if (options.temp_unit === 'F') {
-          cpu_temp = ((cpu_temp*9/5) + 32).toFixed(2)
-        }
-        else if (options.temp_unit === 'C') {}
-        else {
-          cpu_temp = cpu_temp + 273.15
-        }
+        var cpu_temp = (Number(data)/1000 + 273.15).toFixed(2)
         debug(`cpu temp is ${cpu_temp}`)
 
         app.handleMessage(plugin.id, {
@@ -143,26 +124,26 @@ module.exports = function(app) {
             {
               values: [ {
                 path: options.path_cpu_temp,
-                value: cpu_temp
+                value: Number(cpu_temp)
               }]
             }
           ]
         })
       })
 
-      processcputemp.on('error', (error) => {
+      cputemp.on('error', (error) => {
         console.error(error.toString())
       })
 
-      processcputemp.stderr.on('data', function (data) {
+      cputemp.stderr.on('data', function (data) {
         console.error(data.toString())
       })
     }
 
     function getCpuUtil() {
-      var processcpuutilfull = spawn('sh', ['-c', cpu_util_mpstat_command ])
+      var cpuutilfull = spawn('sh', ['-c', cpu_util_mpstat_command ])
 
-      processcpuutilfull.stdout.on('data', (data) => {
+      cpuutilfull.stdout.on('data', (data) => {
         debug(`got cpu utilisation  ${data}`)
         var re = /all/im
         if (data.toString().match(re)) {
@@ -179,13 +160,13 @@ module.exports = function(app) {
               }
               newPath = newPath + "core." + (Number(spl_line[1])+1).toString()
               newPath = newPath + "." + pathArray[(pathArray.length-1)]
-              var cpu_util_core = (100 - Number(spl_line[11])).toFixed(0)
+              var cpu_util_core = ((100 - Number(spl_line[11]))/100).toFixed(2)
               app.handleMessage(plugin.id, {
                 updates: [
                   {
                     values: [ {
                       path: newPath,
-                      value: cpu_util_core
+                      value: Number(cpu_util_core)
                     }]
                   }
                 ]
@@ -193,13 +174,13 @@ module.exports = function(app) {
             }
             else {
               debug(`cpu utilisation is ${spl_line[11]}`)
-              cpu_util_all = (100 - Number(spl_line[11])).toFixed(0)
+              cpu_util_all = ((100 - Number(spl_line[11]))/100).toFixed(2)
               app.handleMessage(plugin.id, {
                 updates: [
                   {
                     values: [ {
                       path: options.path_cpu_util,
-                      value: cpu_util_all
+                      value: Number(cpu_util_all)
                     }]
                   }
                 ]
@@ -209,31 +190,31 @@ module.exports = function(app) {
         }
       })
 
-      processcpuutilfull.on('error', (error) => {
+      cpuutilfull.on('error', (error) => {
         console.error(error.toString())
       })
 
-      processcpuutilfull.stderr.on('data', function (data) {
+      cpuutilfull.stderr.on('data', function (data) {
         console.error(data.toString())
       })
     }
 
     function getMemUtil() {
-      var processmemutil = spawn('sh', ['-c', mem_util_command ])
+      var memutil = spawn('sh', ['-c', mem_util_command ])
 
-      processmemutil.stdout.on('data', (data) => {
+      memutil.stdout.on('data', (data) => {
         debug(`got memory  ${data}`)
         var mem_util = data.toString().replace(/(\n|\r)+$/, '').split('\n')
         mem_util.forEach(function(mem_util_line){
           var splm_line = mem_util_line.replace(/ +/g, ' ').split(' ')
           if (splm_line[0].toString() === "Mem:"){
-            var mem_util_per = (Number(splm_line[2])/Number(splm_line[1])*100).toFixed(0)
+            var mem_util_per = (Number(splm_line[2])/Number(splm_line[1])).toFixed(2)
             app.handleMessage(plugin.id, {
               updates: [
                 {
                   values: [ {
                     path: options.path_mem_util,
-                    value: mem_util_per
+                    value: Number(mem_util_per)
                   }]
                 }
               ]
@@ -242,19 +223,19 @@ module.exports = function(app) {
         })
       })
 
-      processmemutil.on('error', (error) => {
+      memutil.on('error', (error) => {
         console.error(error.toString())
       })
 
-      processmemutil.stderr.on('data', function (data) {
+      memutil.stderr.on('data', function (data) {
         console.error(data.toString())
       })
     }
 
     function getSdUtil() {
-      var processsdutil = spawn('sh', ['-c', sd_util_command ])
+      var sdutil = spawn('sh', ['-c', sd_util_command ])
 
-      processsdutil.stdout.on('data', (data) => {
+      sdutil.stdout.on('data', (data) => {
         debug(`got sd  ${data}`)
         var sd_util = data.toString().replace(/(\n|\r)+$/, '')
         app.handleMessage(plugin.id, {
@@ -262,18 +243,18 @@ module.exports = function(app) {
             {
               values: [ {
                 path: options.path_sd_util,
-                value: sd_util
+                value: Number(sd_util)
               }]
             }
           ]
         })
       })
 
-      processsdutil.on('error', (error) => {
+      sdutil.on('error', (error) => {
         console.error(error.toString())
       })
 
-      processsdutil.stderr.on('data', function (data) {
+      sdutil.stderr.on('data', function (data) {
         console.error(data.toString())
       })
     }
